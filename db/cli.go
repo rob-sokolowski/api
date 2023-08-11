2759fb670893490d1f8ead5611aab7e9b28a8baf
Rob Sokolowski <rpsoko@gmail.com>
2023-08-11 09:08:33 -0400

    I think that's part II

--- db/cli.go
+++ db/cli.go
@@ -6,6 +6,43 @@ import (
 	"os"
 )
 
+func validateMetaCommand(cmd string) error {
+	switch cmd {
+	case ".exit":
+		return nil
+	}
+
+	return fmt.Errorf("unrecognized meta command: %s", cmd)
+}
+
+func doMetaCommand(cmd string) {
+	switch cmd {
+	case ".exit":
+		fmt.Println("adios!")
+		os.Exit(0)
+	}
+}
+
+func validateStatement(cmd string) error {
+	switch cmd {
+	case "select":
+		return nil
+	case "insert":
+		return nil
+	}
+
+	return fmt.Errorf("unrecognized meta command: %s", cmd)
+}
+
+func doStatement(cmd string) {
+	switch cmd {
+	case "select":
+		fmt.Println("TODO: select handling goes here!")
+	case "insert":
+		fmt.Println("TODO: insert handling goes here!")
+	}
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -14,11 +51,21 @@ func main() {
 		scanner.Scan()
 		input := scanner.Text()
 
-		if input == ".exit" {
-			fmt.Println("adios!")
-			break
-		} else {
-			fmt.Println("TODO: Parse the following command: ", input)
+		if input[0] == '.' {
+			err := validateMetaCommand(input)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			doMetaCommand(input)
+			continue
 		}
+
+		err := validateStatement(input)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		doStatement(input)
 	}
 }
